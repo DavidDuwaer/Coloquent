26c921f4f4f1382d7209d507894357b2534ac0c7
David Duwaer <davidduwaer@gmail.com>
2017-09-24 18:27:44 +0200

    code cleanup

--- lib/response/Response.ts
+++ lib/response/Response.ts
@@ -1,11 +1,3 @@
-import {JsonApiResponseBody} from "../JsonApiResponseBody";
-import {JsonApiDoc} from "../JsonApiDoc";
-import {Model} from "../Model";
-import {ToManyRelation} from "../ToManyRelation";
-import {JsonApiStub} from "../JsonApiStub";
-import {Relation} from "../Relation";
-import {ToOneRelation} from "../ToOneRelation";
-import {Map} from "../util/Map";
 
 export abstract class Response
 {
