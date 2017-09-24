fcf39bd311e26c1f10e7825a88fcbcf490834acf
David Duwaer <davidduwaer@gmail.com>
2017-09-24 18:34:19 +0200

    Refactor

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,10 @@
 import {Model as _Model} from "./Model";
 export { _Model as Model }
 
-import {ToManyRelation as _ToManyRelation} from "./ToManyRelation";
+import {ToManyRelation as _ToManyRelation} from "./relation/ToManyRelation";
 export { _ToManyRelation as ToManyRelation }
 
-import {ToOneRelation as _ToOneRelation} from "./ToOneRelation";
+import {ToOneRelation as _ToOneRelation} from "./relation/ToOneRelation";
 export { _ToOneRelation as ToOneRelation }
 
 import {PaginationStrategy as _PaginationStrategy} from "./PaginationStrategy";
--- lib/response/RetrievalResponse.ts
+++ lib/response/RetrievalResponse.ts
@@ -1,10 +1,10 @@
 import {JsonApiResponseBody} from "../JsonApiResponseBody";
 import {JsonApiDoc} from "../JsonApiDoc";
 import {Model} from "../Model";
-import {ToManyRelation} from "../ToManyRelation";
+import {ToManyRelation} from "../relation/ToManyRelation";
 import {JsonApiStub} from "../JsonApiStub";
-import {Relation} from "../Relation";
-import {ToOneRelation} from "../ToOneRelation";
+import {Relation} from "../relation/Relation";
+import {ToOneRelation} from "../relation/ToOneRelation";
 import {Map} from "../util/Map";
 import {Response} from "./Response";
 
