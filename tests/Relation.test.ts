4134f49c8127c624db167c7b05608399e3f765af
David Duwaer <davidduwaer@gmail.com>
2017-09-24 19:06:24 +0200

    Added test

---
+++ tests/Relation.test.ts
@@ -0,0 +1,23 @@
+import {assert, expect} from 'chai';
+import * as moxios from 'moxios';
+import {Hero} from './dummy/Hero';
+import {Builder} from '../lib/Builder';
+import {PaginationStrategy} from "../lib/PaginationStrategy";
+import {PluralResponse} from "../lib/response/PluralResponse";
+
+describe('Relation', () => {
+    let model;
+
+    beforeEach(() => {
+        moxios.install();
+        model = new Hero();
+    });
+
+    afterEach(() => {
+        moxios.uninstall();
+    });
+
+    it('successufly stores its referring object', () => {
+        assert.equal(model, model.friends().getReferringObject());
+    });
+});
