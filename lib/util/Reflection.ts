05b880efd9906d8d8dff21bb20d4be1d52b39a22
David Duwaer <davidduwaer@gmail.com>
2018-04-02 14:00:44 +0200

    Expanded stacktrace matching compatibility #33

--- lib/util/Reflection.ts
+++ lib/util/Reflection.ts
@@ -2,7 +2,7 @@ export class Reflection
 {
     public static getNameOfNthMethodOffStackTrace(error: Error, n: number): string
     {
-        let re = /@|at [a-zA-Z0-9\._]+\.([a-zA-Z0-9_]+) \(/g;
+        let re = /@|at [a-zA-Z0-9\.\/_]+\.([a-zA-Z0-9_]+) \(/g;
         let stack = error.stack;
         for (let i: number = 0; i < n - 1; i++) {
             re.exec(stack);
