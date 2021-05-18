54137c2314a9c3877f53bf843942ac8fbb6ee9dd
David Duwaer <davidduwaer@gmail.com>
2021-05-18 11:31:01 +0200

    getNameOfNthMethodOffStackTrace method was still returning e.g. 'Hero.friends' instead of 'friends' as a method name, so added a regex that fetches only the characters occurring after the last dot in the name

--- src/util/Reflection.ts
+++ src/util/Reflection.ts
@@ -4,7 +4,19 @@ export class Reflection
 {
     public static getNameOfNthMethodOffStackTrace(error: Error, n: number): string | undefined
     {
-        const parsed = ErrorStackParser.parse(error)
-        return (parsed.length >= n) ? parsed[n-1].functionName : undefined;
+        const parsed = ErrorStackParser.parse(error);
+        const functionName = parsed.length >= n
+            ? parsed[n-1].functionName
+            : undefined
+        return functionName !== undefined
+            ? getMethodName(functionName)
+            : undefined;
     }
+}
+
+function getMethodName(functionName: string)
+{
+    const matcher = functionName.match(/[^.]+$/);
+    if (matcher !== null && matcher.length > 0)
+        return matcher[0];
 }
